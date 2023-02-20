e81a2e5ff6e9d72e849ccefe89917bba8fccb714
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-02-20 12:00:33 +0100

    Fix

--- app/Resources/Customer/CustomerPagination.php
+++ app/Resources/Customer/CustomerPagination.php
@@ -1,27 +1,27 @@
 <?php
 
 namespace App\Resources\Customer;
-
 use App\Resources\Customer\Customer as CustomerResource;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-
 class CustomerPagination extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
+        $variantItems = collect($this->data);
+
         return [
             'currentPage' => $this->current_page,
             'perPage' => $this->per_page,
             'total' => $this->total,
             'lastPage' => $this->last_page,
-            'customers' => $this->data
+            'customers' => CustomerResource::collection($variantItems)
         ];
     }
 }
