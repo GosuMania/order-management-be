0f953a817bad4bc11feba9a3a239def275151764
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-02-28 17:06:47 +0100

    Fix Clothing

--- app/Http/Controllers/ClothingSizeController.php
+++ app/Http/Controllers/ClothingSizeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ClothingSize;
 use App\Http\Controllers\Controller;
-use App\Resources\ClothingSize\ShoeSize as ClothingSizeResource;
+use App\Resources\ClothingSize\ClothingSize as ClothingSizeResource;
 use Illuminate\Http\Request;
 
 class ClothingSizeController extends Controller
