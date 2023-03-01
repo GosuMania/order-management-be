38bf7e2f9cad7d2b76736ca75ff6d81b235649ec
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-03-01 13:41:46 +0100

    Fix Carbon Import

--- app/Http/Controllers/ImageProductController.php
+++ app/Http/Controllers/ImageProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ImageProduct;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ImageProductController extends Controller
 {
