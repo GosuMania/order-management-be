f923a9b55d13d26cd13f437b302a5b26e6d2fd8c
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-03-01 13:44:15 +0100

    Configurato path images

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,7 +55,13 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-
+        'images' => [
+            'driver' => 'local',
+            'root' => storage_path('app/images'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
     ],
 
     /*
