49bbf198ebabe1b9e8204359eaad79f57ba5192a
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-03-07 19:08:56 +0100

    Test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
     {
         $products = ProductResource::collection(Product::orderBy($orderBy, $ascDesc)->paginate($perPage, ['*'], 'page', $page));
         $products[0]->descFornitore = "ciao";
-        return ProductResource::collection(Product::orderBy($orderBy, $ascDesc)->paginate($perPage, ['*'], 'page', $page));
+        return $products;
     }
 
     /**
