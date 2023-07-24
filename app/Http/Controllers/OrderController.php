54e7cb34dba34c5144d6044afb460952c71d45fa
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-07-24 17:23:03 +0200

    Create Update Order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -51,30 +51,83 @@ class OrderController extends Controller
         return new OrderResource($order);
     }
 
-    public function getProductListByIdProduct($id) {
+    public function getProductListByIdProduct($id)
+    {
         $orderProducts =
             OrderProduct::join('product_variants', 'product_variants.id', '=', 'order_products.id_product_variant')
-            ->join('products', 'products.id', '=', 'order_products.id_product')
-            -> where('order_products.id_product', $id)->orderBy('order_products.id', 'ASC')
-            ->get();
-        return  OrderProductResource::collection($orderProducts);
+                ->join('products', 'products.id', '=', 'order_products.id_product')
+                ->where('order_products.id_product', $id)->orderBy('order_products.id', 'ASC')
+                ->get();
+        return OrderProductResource::collection($orderProducts);
     }
 
-    public function getTotalPiecesAndAmounts() {
+    public function getTotalPiecesAndAmounts()
+    {
         $orders = Order::select(
             'total_pieces',
             'total_amount'
         )->get();
         $totalPieces = 0;
-        $totalAmount= 0;
+        $totalAmount = 0;
         foreach ($orders as $obj) {
             $totalPieces = $totalPieces + $obj['total_pieces'];
-            $totalAmount =  $totalAmount + $obj['total_amount'];
+            $totalAmount = $totalAmount + $obj['total_amount'];
         }
         $data['totalPieces'] = $totalPieces;
         $data['totalAmount'] = $totalAmount;
         return response()->json(["data" => $data], 200);
 
+    }
+
+    public function createOrUpdate(Request $request)
+    {
+        $object = Order::updateOrCreate(
+            ['id' => $request->id],
+            [
+                'id_provider' => $request->idProvider,
+                'desc_provider' => $request->descProvider,
+                'id_product_type' => $request->idProductType,
+                'desc_product_type' => $request->descProductType,
+                'immagine' => $request->image,
+                'codice_articolo' => $request->productCode,
+                'descrizione_articolo' => $request->productDesc,
+                'prezzo' => $request->price,
+                'date' => Carbon::now()
+            ]
+        );
+        OrderProduct::where('id_product', $request->id)->delete();
+        foreach ($request->productList as $product) {
+            foreach ($product->colorVariants as $colorVariant) {
+                if (count($colorVariant['sizeVariants']) > 0) {
+                    foreach ($colorVariant['sizeVariants'] as $sizeVariant) {
+                        $objectOrderProduct = OrderProduct::create(
+                            [
+                                'id_order' => $object->id,
+                                'id_product' => $product->id,
+                                'id_product_variant' => $sizeVariant->id,
+                                'quantity' => $sizeVariant->stock,
+                                'date' => Carbon::now()
+                            ]
+                        );
+                    }
+                } else {
+                    $objectOrderProduct = OrderProduct::create(
+                        [
+                            'id_order' => $object->id,
+                            'id_product' => $product->id,
+                            'id_product_variant' => $colorVariant->id,
+                            'quantity' => $colorVariant->stock,
+                            'date' => Carbon::now()
+                        ]
+                    );
+                }
+            }
+
+        }
+        $id = $object->id;
+        $object = $request;
+        $object['id'] = $id;
+        return response()->json(['data' => new OrderResource($object)], 200);
     }
 
     public function delete($id)
