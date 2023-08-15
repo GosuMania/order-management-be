c891c9b5a19d49bd7d70e483f37e6776e5c12fb7
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-08-15 23:44:02 +0200

    Test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ClothingNumberSize;
+use App\Models\ClothingSize;
+use App\Models\Color;
 use App\Models\Order;
 use App\Models\OrderProduct;
+use App\Models\ProductType;
+use App\Models\ProductVariant;
+use App\Models\Provider;
+use App\Models\ShoeSize;
 use App\Resources\Order\Order as OrderResource;
 use App\Resources\Order\OrderProduct as OrderProductResource;
 use App\Resources\Product\ProductOrder as ProductOrderResource;
@@ -60,6 +67,14 @@ class OrderController extends Controller
             ->join('products', 'products.id', '=', 'order_products.id_product')
             ->join('product_variants', 'product_variants.id', '=', 'order_products.id_product_variant')
             ->get();
+        $providers = Provider::orderBy('id', 'ASC')->get();
+        $productTypes = ProductType::orderBy('id', 'ASC')->get();
+        $colors = Color::orderBy('id', 'ASC')->get();
+        $showSizes = ShoeSize::orderBy('id', 'ASC')->get();
+        $clothingSizes = ClothingSize::orderBy('id', 'ASC')->get();
+        $clothingNumberSizes = ClothingNumberSize::orderBy('id', 'ASC')->get();
+        $orderProductsNew = $this->groupAndMergeVariants($orderProducts, $providers, $productTypes, $colors, $showSizes, $clothingSizes, $clothingNumberSizes, $orderProductsNew);
+
         return ProductOrderResource::collection($orderProducts);
         /*
         $orderProducts =
@@ -69,6 +84,120 @@ class OrderController extends Controller
                 ->get();
         return OrderProductResource::collection($orderProducts);
         */
+    }
+
+    public function groupAndMergeVariants($inputArray, $providers, $productTypes, $colors, $showSizes, $clothingSizes, $clothingNumberSizes, $orderProductsNew)
+    {
+        $groupedArray = [];
+
+        // Raggruppa gli oggetti in base all'id_product
+        $groupedProducts = collect($inputArray)->groupBy('id_product');
+
+        foreach ($groupedProducts as $id_product => $products) {
+            $mergedProduct = $products[0]; // Prendi il primo oggetto come base per il merge
+            /*
+            $colorVariants = [];
+
+            foreach ($products as $product) {
+                $colorVariant = [
+                    'id' => $product['id'],
+                    'id_product_variant' => $product['id_product_variant'],
+                    'quantity' => $product['quantity'],
+                    'stock' => $product['stock'],
+                ];
+
+                $colorVariants[] = $colorVariant;
+            }
+            */
+
+            $mergedProduct['color_variants'] = $this->getColorVariants($products, $mergedProduct['id_product_type'], $colors, $showSizes, $clothingSizes, $clothingNumberSizes);
+            $groupedArray[] = $mergedProduct;
+        }
+
+        return $groupedArray;
+    }
+
+    public function getColorVariants($productVariants, $idProductType, $colors, $showSizes, $clothingSizes, $clothingNumberSizes)
+    {
+        $colorVariants = [];
+        $sizeVariants = [];
+        $i = -1;
+        $j = 0;
+        foreach ($productVariants as $productVariant) {
+            if ($i == -1) {
+                $i = $i + 1;
+                $colorVariants[$i]['id'] = $productVariant['id_color'];
+                $colorVariants[$i]['descColor'] = $this->getDescColor($colors, $productVariant['id_color']);
+            } else if ($colorVariants[$i]['id'] != $productVariant['id_color']) {
+                $colorVariants[$i]['sizeVariants'] = $sizeVariants;
+                $i = $i + 1;
+                $colorVariants[$i]['id'] = $productVariant['id_color'];
+                $colorVariants[$i]['descColor'] = $this->getDescColor($colors, $productVariant['id_color']);
+                $sizeVariants = []; // inizialitto di nuovo array sizeVariants
+                $j = 0; // azzero contatore array sizeVariant
+            } else {
+                $j = $j + 1;
+            }
+
+            switch ($idProductType) {
+                case 0:
+                    if ($productVariant['id_clothing_size_type'] == 1) {
+                        if (isset($clothingSizes[$productVariant['id_clothing_size']])) {
+                            $sizeVariants[$j]['id'] = $productVariant['id_clothing_size'];
+                            $sizeVariants[$j]['idProductVariant'] = $productVariant['id'];
+                            $sizeVariants[$j]['descSize'] = $this->getDescSize($clothingSizes, $productVariant['id_clothing_size']);
+                            $sizeVariants[$j]['stock'] = $productVariant['stock'];
+                            $sizeVariants[$j]['stockOrder'] = $productVariant['quantity'];
+                        }
+                    } else {
+                        if (isset($clothingNumberSizes[$productVariant['id_clothing_number_size']])) {
+                            $sizeVariants[$j]['id'] = $productVariant['id_clothing_number_size'];
+                            $sizeVariants[$j]['idProductVariant'] = $productVariant['id'];
+                            $sizeVariants[$j]['descSize'] = $this->getDescSize($clothingNumberSizes, $productVariant['id_clothing_number_size']);
+                            $sizeVariants[$j]['stock'] = $productVariant['stock'];
+                            $sizeVariants[$j]['stockOrder'] = $productVariant['quantity'];
+                        }
+                    }
+                    break;
+                case 2:
+                    if (isset($showSizes[$productVariant['id_shoe_size']])) {
+                        $sizeVariants[$j]['id'] = $productVariant['id_shoe_size'];
+                        $sizeVariants[$j]['idProductVariant'] = $productVariant['id'];
+                        $sizeVariants[$j]['descSize'] = $this->getDescSize($showSizes, $productVariant['id_shoe_size']);
+                        $sizeVariants[$j]['stock'] = $productVariant['stock'];
+                        $sizeVariants[$j]['stockOrder'] = $productVariant['quantity'];
+                    }
+                    break;
+                default:
+                    $colorVariants[$i]['idProductVariant'] = $productVariant['id'];
+                    $colorVariants[$i]['stock'] = $productVariant['stock'];
+                    $colorVariants[$j]['stockOrder'] = $productVariant['quantity'];
+            }
+        }
+        if (count($colorVariants) > 0) {
+            $colorVariants[$i]['sizeVariants'] = $sizeVariants;
+        }
+        return $colorVariants;
+    }
+
+    public function getDescColor($colors, $id)
+    {
+        foreach ($colors as $color) {
+            if ($color['id'] == $id) {
+                return $color['colore'];
+            }
+        }
+        return '';
+    }
+
+    public function getDescSize($sizes, $id)
+    {
+        foreach ($sizes as $size) {
+            if ($size['id'] == $id) {
+                return $size['desc'];
+            }
+        }
+        return '';
     }
 
     public function getTotalPiecesAndAmounts()
