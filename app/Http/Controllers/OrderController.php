320d736717d27279bbb606b2eedcc457c478343f
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-09-06 11:17:18 +0200

    fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -73,8 +73,8 @@ class OrderController extends Controller
             ->join('products', 'products.id', '=', 'order_products.id_product')
             ->where('products.id_provider', $idProvider)
             ->join('product_variants', 'product_variants.id_product', '=', 'products.id')
-            ->select('orders.id')
-            ->orderBy('orders.id', 'ASC');
+            ->select('orders.*', 'products.*', 'product_variants.*')
+            ->orderBy('orders.id', 'ASC')->get();
         return response()->json(['data' => $orders], 200);
     }
 
