c4bdb38b5b17a4695c85a8dabb5f5b0f8c30f3d8
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-11-05 21:29:19 +0100

    Fix stats

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -416,7 +416,7 @@ class OrderController extends Controller
                             'id_order' => $objectOrder->id,
                             'id_product' => $product['id'],
                             'id_product_variant' => $sizeVariant['idProductVariant'],
-                            'id_season' => $objectOrder->id_season,
+                            'id_season' => $request->idSeason,
                             'quantity' => $sizeVariant['stockOrder'],
                             'date' => Carbon::now()
                         ]);
@@ -431,7 +431,7 @@ class OrderController extends Controller
                         'id_order' => $objectOrder->id,
                         'id_product' => $product['id'],
                         'id_product_variant' => $colorVariant['idProductVariant'],
-                        'id_season' => $objectOrder->id_season,
+                        'id_season' => $request->idSeason,
                         'quantity' => $colorVariant['stockOrder'],
                         'date' => Carbon::now()
                     ]);
