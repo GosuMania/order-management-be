df6a32c01e7a2e7416c414e91d5cbbf42d0a4cf6
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-11-05 19:37:21 +0100

    Fix stats

--- routes/api.php
+++ routes/api.php
@@ -152,7 +152,7 @@ Route::controller(OrderController::class)->prefix('order')->group(function () {
 });
 
 Route::controller(OrderProductController::class)->prefix('order-product')->group(function () {
-    Route::get('get-order-product-stats', 'getOrderProductStats');
+    Route::get('get-order-product-stats/{idProvider}', 'getOrderProductStats');
     Route::get('get-by-id-order', 'getByIdOrder'); // restituisce la lista
     Route::post('create-or-update', 'createOrUpdate'); // crea o modifica
     Route::get('get-by-id/{id}', 'getById'); // restituisce una specifica
